824a01e8e946f7457d32fd35490767825e8580e6
sebastian101 <sebastianduq.80@gmail.com>
2016-03-24 12:33:44 -0500

    Primera Sección GUI
    Primera Sección GUI

---
+++ src/GUI_3_8/Dialogo1.java
@@ -0,0 +1,16 @@
+package GUI_3_8;
+
+import javax.swing.JOptionPane;
+
+// Uso de JoptionPane para imprimir varias lineas en un cuadro de dialogo
+
+public class Dialogo1 {
+
+	public static void main(String[] args) {
+		
+		// muestra un cuadro de dialogo con un mensaje
+		JOptionPane.showMessageDialog(null, "Bienvenido \na\nJava");
+
+	} // fin main
+
+} // fin Clase Dialogo1 
